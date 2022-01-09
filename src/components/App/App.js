a81c8e818d5f5037f906d023a5e2247e8a0603e1
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2022-01-09 19:09:58 +0100

    add src

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/List';
 
+
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
+        <List title={['Things to do ', <sup key='1'>soon!</sup>]} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png">
+        </List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+        src: PropTypes.string,
+    }
+    
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
+
     render() {
         return (
             <main className={styles.component}>
-
-
                 <section className={styles.component}>
-                    <Hero />
+                    <Hero titleText={this.props.title} src={this.props.src} />
+                    <div className={styles.description}>
+                        {this.props.children}
+                    </div>
                 </section>
-
             </main>
         )
     }
